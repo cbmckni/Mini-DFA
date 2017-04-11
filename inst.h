46e8a24c6496dea7c092e2d49a59290416b6258d
colmc11 <cbmckni@clemson.edu>
2017-04-11 09:06:25 -0400

    oops, make sure you get this file

---
+++ inst.h
@@ -0,0 +1,30 @@
+/**********************************************************************
+* Author: Cole McKnight
+* File: inst.h
+* Modified On: 4/10/17
+* Description: header for Inst, which represents a single MIPS instruction.
+***********************************************************************/
+#ifndef INST_H
+#define INST_H
+
+#include <stdio.h>
+#include <math.h>
+#include <string>
+using namespace std;
+
+class Inst{
+public:
+    int index; //index of instruction in program
+    string fullI; //full instruction(for printing)
+    int regs[3]; //registers used 
+   	
+    Inst(int,string); 
+       
+    void print();
+ 
+};
+
+
+#endif
+
+
